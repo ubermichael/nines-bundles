3cd4501e990d591408478f243372d2e378318354
Michael Joyce <ubermichael@gmail.com>
2017-04-04 09:18:36 -0700

    start configuring nines_user bundle.

--- UserBundle/DependencyInjection/Configuration.php
+++ UserBundle/DependencyInjection/Configuration.php
@@ -20,6 +20,13 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('nines_user');
 
+        $rootNode
+            ->children()
+                ->arrayNode('permission_levels')
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end();
+        
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
