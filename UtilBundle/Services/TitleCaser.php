fb9d32c07e59bb55f67944edfab98138e739baed
Michael Joyce <ubermichael@gmail.com>
2017-10-13 14:43:07 -0700

    Remove the short-word special cases from the state abbrs to capitalize.

--- UtilBundle/Services/TitleCaser.php
+++ UtilBundle/Services/TitleCaser.php
@@ -59,13 +59,13 @@ class TitleCaser {
      * @var array
      */
     private $states = array(
-        'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'PEI',
+        'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'PE', 'PEI',
         'QC', 'SK', 'YT',
         'US', 'USA',
-        'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI',
-        'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN',
-        'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH',
-        'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
+        'AL', 'AK', 'AZ', 'AR', 'CA', 'CT', 'DC', 'FL', 'GA', 'HI',
+        'ID', 'IL', 'IA', 'KS', 'KY', 'MD', 'MA', 'MI', 'MN',
+        'MS', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+        'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
         'WV', 'WI', 'WY',
     );
     
