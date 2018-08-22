1a22b07e16aff81a1fb90bd376fc056e17a8bd3a
Michael Joyce <ubermichael@gmail.com>
2018-08-22 16:05:04 -0700

    more tests.

--- FeedbackBundle/Tests/Controller/CommentNoteControllerTest.php
+++ FeedbackBundle/Tests/Controller/CommentNoteControllerTest.php
@@ -16,7 +16,7 @@ class CommentNoteControllerTest extends BaseTestCase
             LoadCommentNote::class
         ];
     }
-    
+
     /**
      * @group anon
      * @group index
@@ -24,10 +24,10 @@ class CommentNoteControllerTest extends BaseTestCase
     public function testAnonIndex() {
         $client = $this->makeClient();
         $crawler = $client->request('GET', '/admin/comment_note/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
         $this->assertEquals(0, $crawler->selectLink('New')->count());
     }
-    
+
     /**
      * @group user
      * @group index
@@ -38,7 +38,7 @@ class CommentNoteControllerTest extends BaseTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals(0, $crawler->selectLink('New')->count());
     }
-    
+
     /**
      * @group admin
      * @group index
@@ -47,267 +47,7 @@ class CommentNoteControllerTest extends BaseTestCase
         $client = $this->makeClient(LoadUser::ADMIN);
         $crawler = $client->request('GET', '/admin/comment_note/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertEquals(1, $crawler->selectLink('New')->count());
-    }
-    
-    /**
-     * @group anon
-     * @group show
-     */
-    public function testAnonShow() {
-        $client = $this->makeClient();
-        $crawler = $client->request('GET', '/admin/comment_note/1');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertEquals(0, $crawler->selectLink('Edit')->count());
-        $this->assertEquals(0, $crawler->selectLink('Delete')->count());
-    }
-    
-    /**
-     * @group user
-     * @group show
-     */
-    public function testUserShow() {
-        $client = $this->makeClient(LoadUser::USER);
-        $crawler = $client->request('GET', '/admin/comment_note/1');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertEquals(0, $crawler->selectLink('Edit')->count());
-        $this->assertEquals(0, $crawler->selectLink('Delete')->count());
-    }
-    
-    /**
-     * @group admin
-     * @group show
-     */
-    public function testAdminShow() {
-        $client = $this->makeClient(LoadUser::ADMIN);
-        $crawler = $client->request('GET', '/admin/comment_note/1');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertEquals(1, $crawler->selectLink('Edit')->count());
-        $this->assertEquals(1, $crawler->selectLink('Delete')->count());
-    }
-
-    /**
-     * @group anon
-     * @group typeahead
-     */
-    public function testAnonTypeahead() {
-        $client = $this->makeClient();
-        $client->request('GET', '/admin/comment_note/typeahead?q=STUFF');
-        $response = $client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('application/json', $response->headers->get('content-type'));
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );        
-        $json = json_decode($response->getContent());
-        $this->assertEquals(4, count($json));
-    }
-
-    /**
-     * @group user
-     * @group typeahead
-     */
-    public function testUserTypeahead() {
-        $client = $this->makeClient(LoadUser::USER);
-        $client->request('GET', '/admin/comment_note/typeahead?q=STUFF');
-        $response = $client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('application/json', $response->headers->get('content-type'));
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );        
-        $json = json_decode($response->getContent());
-        $this->assertEquals(4, count($json));
-    }
-
-    /**
-     * @group admin
-     * @group typeahead
-     */
-    public function testAdminTypeahead() {
-        $client = $this->makeClient(LoadUser::ADMIN);
-        $client->request('GET', '/admin/comment_note/typeahead?q=STUFF');
-        $response = $client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('application/json', $response->headers->get('content-type'));
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );        
-        $json = json_decode($response->getContent());
-        $this->assertEquals(4, count($json));
-    }
-    /**
-     * @group anon
-     * @group edit
-     */
-    public function testAnonEdit() {
-        $client = $this->makeClient();
-        $crawler = $client->request('GET', '/admin/comment_note/1/edit');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect());
-    }
-    
-    /**
-     * @group user
-     * @group edit
-     */
-    public function testUserEdit() {
-        $client = $this->makeClient(LoadUser::USER);
-        $crawler = $client->request('GET', '/admin/comment_note/1/edit');
-        $this->assertEquals(403, $client->getResponse()->getStatusCode());
-    }
-    
-    /**
-     * @group admin
-     * @group edit
-     */
-    public function testAdminEdit() {
-        $client = $this->makeClient(LoadUser::ADMIN);
-        $formCrawler = $client->request('GET', '/admin/comment_note/1/edit');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );        
-        $form = $formCrawler->selectButton('Update')->form([
-            // DO STUFF HERE.
-            // 'admin/comment_notes[FIELDNAME]' => 'FIELDVALUE',
-        ]);
-        
-        $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirect('/admin/comment_note/1'));
-        $responseCrawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // $this->assertEquals(1, $responseCrawler->filter('td:contains("FIELDVALUE")')->count());
-    }
-    
-    /**
-     * @group anon
-     * @group new
-     */
-    public function testAnonNew() {
-        $client = $this->makeClient();
-        $crawler = $client->request('GET', '/admin/comment_note/new');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect());
-    }
-    
-    /**
-     * @group anon
-     * @group new
-     */
-    public function testAnonNewPopup() {
-        $client = $this->makeClient();
-        $crawler = $client->request('GET', '/admin/comment_note/new_popup');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect());
-    }
-    
-    /**
-     * @group user
-     * @group new
-     */
-    public function testUserNew() {
-        $client = $this->makeClient(LoadUser::USER);
-        $crawler = $client->request('GET', '/admin/comment_note/new');
-        $this->assertEquals(403, $client->getResponse()->getStatusCode());
-    }
-
-    /**
-     * @group user
-     * @group new
-     */
-    public function testUserNewPopup() {
-        $client = $this->makeClient(LoadUser::USER);
-        $crawler = $client->request('GET', '/admin/comment_note/new_popup');
-        $this->assertEquals(403, $client->getResponse()->getStatusCode());
-    }
-
-    /**
-     * @group admin
-     * @group new
-     */
-    public function testAdminNew() {
-        $client = $this->makeClient(LoadUser::ADMIN);
-        $formCrawler = $client->request('GET', '/admin/comment_note/new');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );        
-        $form = $formCrawler->selectButton('Create')->form([
-            // DO STUFF HERE.
-            // 'admin/comment_notes[FIELDNAME]' => 'FIELDVALUE',
-        ]);
-        
-        $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $responseCrawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // $this->assertEquals(1, $responseCrawler->filter('td:contains("FIELDVALUE")')->count());
-    }
-    
-    /**
-     * @group admin
-     * @group new
-     */
-    public function testAdminNewPopup() {
-        $client = $this->makeClient(LoadUser::ADMIN);
-        $formCrawler = $client->request('GET', '/admin/comment_note/new_popup');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );        
-        $form = $formCrawler->selectButton('Create')->form([
-            // DO STUFF HERE.
-            // 'admin/comment_notes[FIELDNAME]' => 'FIELDVALUE',
-        ]);
-        
-        $client->submit($form);
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $responseCrawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // $this->assertEquals(1, $responseCrawler->filter('td:contains("FIELDVALUE")')->count());
-    }
-    
-    /**
-     * @group anon
-     * @group delete
-     */
-    public function testAnonDelete() {
-        $client = $this->makeClient();
-        $crawler = $client->request('GET', '/admin/comment_note/1/delete');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect());
-    }
-    
-    /**
-     * @group user
-     * @group delete
-     */
-    public function testUserDelete() {
-        $client = $this->makeClient(LoadUser::USER);
-        $crawler = $client->request('GET', '/admin/comment_note/1/delete');
-        $this->assertEquals(403, $client->getResponse()->getStatusCode());
-    }
-
-    /**
-     * @group admin
-     * @group delete
-     */
-    public function testAdminDelete() {
-        $preCount = count($this->em->getRepository(CommentNote::class)->findAll());
-        $client = $this->makeClient(LoadUser::ADMIN);
-        $crawler = $client->request('GET', '/admin/comment_note/1/delete');
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $responseCrawler = $client->followRedirect();
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        
-        $this->em->clear();
-        $postCount = count($this->em->getRepository(CommentNote::class)->findAll());
-        $this->assertEquals($preCount - 1, $postCount);
+        $this->assertEquals(0, $crawler->selectLink('New')->count());
     }
 
 }
