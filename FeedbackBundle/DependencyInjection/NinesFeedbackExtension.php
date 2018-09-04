41d41f4379f11d51c4b7eb94b1a204a27f13189d
Michael Joyce <ubermichael@gmail.com>
2018-09-04 10:52:53 -0700

    fix comment service def.

--- FeedbackBundle/DependencyInjection/NinesFeedbackExtension.php
+++ FeedbackBundle/DependencyInjection/NinesFeedbackExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 /**
  * This is the class that loads and manages your bundle configuration.
  *
- * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ * @link https://symfony.com/doc/3.4/bundles/extension.html
  */
 class NinesFeedbackExtension extends Extension {
 
