82e19fc26dec992fa9e08981807c0952760bb96b
Michael Joyce <ubermichael@gmail.com>
2019-06-10 11:14:24 -0700

    update tests.

--- EditorBundle/Tests/Controller/DefaultControllerTest.php
+++ EditorBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertStringContainsStringIgnoringCase('Hello World', $client->getResponse()->getContent());
     }
 }
