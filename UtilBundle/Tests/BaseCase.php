a6600d298ae0312b92a653045d91bb01d8408f5b
Michael Joyce <ubermichael@gmail.com>
2020-01-16 13:42:21 -0800

    make getReference more flexinble

--- UtilBundle/Tests/BaseCase.php
+++ UtilBundle/Tests/BaseCase.php
@@ -11,7 +11,11 @@ declare(strict_types=1);
 namespace Nines\UtilBundle\Tests;
 
 use Doctrine\Common\DataFixtures\ReferenceRepository;
+use Doctrine\Common\Persistence\Mapping\MappingException as MappingException;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\TransactionRequiredException;
 use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -28,16 +32,41 @@ abstract class BaseCase extends WebTestCase {
      */
     protected $references;
 
+    /**
+     * Get a list of fixture classes to load.
+     *
+     * @return array
+     */
     protected function fixtures() : array {
         return [];
     }
 
-    protected function getReference(string $id) {
-        if ($this->references->hasReference($id)) {
-            return $this->references->getReference($id);
+    /**
+     * Get one data fixture. If $reload is true, the fixture will
+     * be fetched from the database.
+     *
+     * @param string $id
+     * @param bool $reload
+     *
+     * @return object|null
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws TransactionRequiredException
+     */
+    protected function getReference(string $id, $reload = false) {
+        if( ! $this->references->hasReference($id)) {
+            return null;
         }
+        $object = $this->references->getReference($id);
+        if ( ! $reload) {
+            return $object;
+        }
+        return $this->entityManager->find(get_class($object), $object->getId());
     }
 
+    /**
+     * Set up the container and fixtures.
+     */
     protected function setUp() : void {
         parent::setUp();
         self::bootKernel();
@@ -45,6 +74,11 @@ abstract class BaseCase extends WebTestCase {
         $this->references = $this->loadFixtures($this->fixtures())->getReferenceRepository();
     }
 
+    /**
+     * Clear out the memory for the next test run.
+     *
+     * @throws MappingException
+     */
     protected function tearDown() : void {
         if ($this->entityManager) {
             $this->entityManager->clear();
