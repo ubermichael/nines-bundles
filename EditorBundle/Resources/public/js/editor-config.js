09adc9bec608ee1cd99d5a7579ce64b8fb90dacf
Michael Joyce <ubermichael@gmail.com>
2020-11-23 14:39:27 -0800

    improve the editor ui.

--- EditorBundle/Resources/public/js/editor-config.js
+++ EditorBundle/Resources/public/js/editor-config.js
@@ -3,15 +3,18 @@ function getTinyMceConfig(editorUploadPath) {
     return {
         branding: false,
         selector: '.tinymce',
-        plugins: 'hr image imagetools link lists paste wordcount',
+        plugins: 'advlist anchor charmap code help hr image imagetools link ' +
+            'lists paste preview searchreplace table wordcount',
         relative_urls: false,
         convert_urls: false,
-        height: 480,
-        menubar: 'edit insert view format tools',
+        height: 320,
+        menubar: 'edit insert view format table tools help',
 
-        toolbar: "undo redo | styleselect | paste | bold italic | alignleft "
-            + "aligncenter alignright alignjustify | "
-            + "bullist numlist outdent indent | link image",
+        toolbar: [
+            'undo redo | styleselect | pastetext | bold italic | alignleft aligncenter alignright alignjustify | table',
+            'bullist numlist | outdent indent | link | charmap | code'],
+
+        browser_spellcheck: true,
 
         image_caption: true,
         images_upload_url: editorUploadPath,
@@ -19,14 +22,18 @@ function getTinyMceConfig(editorUploadPath) {
         image_advtab: true,
         image_title: true,
 
+        resize: true,
+        paste_as_text: true,
+        paste_block_drop: true,
+
         style_formats_merge: true,
         style_formats: [{
                 title: 'Image Left',
                 selector: 'img, figure',
                 styles: {
                     'float': 'left',
-                    'margin': '0 10px 0 10px'
-                }
+                    'margin': '0 10px 0 10px',
+                },
             },
             {
                 title: 'Image Center',
@@ -34,17 +41,18 @@ function getTinyMceConfig(editorUploadPath) {
                 styles: {
                     position: 'relative',
                     transform: 'translateX(-50%)',
-                    left: '50%'
-                }
+                    left: '50%',
+                },
             },
             {
                 title: 'Image Right',
                 selector: 'img, figure',
                 styles: {
                     'float': 'right',
-                    'margin': '0 10px 0 10px'
-                }
-            }]
+                    'margin': '0 10px 0 10px',
+                },
+            },
+        ],
     };
 
 }
